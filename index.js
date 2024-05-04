c57307cf53c70d533c7c11a58930415f126b4202
RicoDi <d.mamalkov@gmail.com>
2024-05-04 13:44:40 +0300

    modified:   index.js 	new file:   public/authorization.html 	new file:   public/images/icons/icons8-whatsapp-48.svg 	new file:   public/images/icons/telegram-48.svg 	new file:   public/images/icons/telegram-96.svg 	new file:   public/images/icons/telegram.json 	new file:   public/images/icons/viber-48.svg 	new file:   public/images/icons/viber-96.svg 	new file:   public/images/icons/whatsapp-96.svg 	modified:   public/index.html 	new file:   public/stylesheets/authorization.css 	modified:   public/stylesheets/style.css

--- index.js
+++ index.js
@@ -1,17 +1,22 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-// const bootstrap = require('bootstrap');
 
+// Создание методов для вывода статических файлов, таких как css, js, images, fonts, etc.
 app.use(express.static(path.join(__dirname, 'public')));
-
 app.use('/css', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/css')))
 app.use('/js', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/js')))
 app.use('/js', express.static(path.join(__dirname, 'node_modules/jquery/dist')))
 app.use('/js', express.static(path.join(__dirname, 'node_modules/inputmask/dist')))
 
+// Создание маршрута для index страницы
 app.get('/', (req, res) => {
     res.sendFile(`${__dirname}./public/index.html`);
+});
+
+// Создание маршрута для authorization страницы
+app.get('/authorization', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/authorization.html'));
 });
 
 app.listen(3333, () => {
